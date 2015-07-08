cab1fc0f9390efb82339a3f6f5cff03ab09bd1a0
dasteere <dasteere@live.com>
2015-07-07 17:19:31 -0700

    Added tests and fixed update

--- Main.py
+++ Main.py
@@ -1,31 +1,32 @@
 import sqlite3
 import datetime
 
+
 # given a username and reputation, add the user to the db if the username is unique, otherwise update their reputation.
 # returns the userid of the user
 def insertUser(username, dailyRep, totalRep):
     data = c.execute('SELECT * FROM user WHERE name LIKE (?)', [username]).fetchall()
     if data:
-        c.execute('UPDATE user SET dailyRep=(?) AND totalRep=(?) WHERE name LIKE (?)',
-                        [dailyRep, totalRep, username])
+        c.execute('UPDATE user SET dailyRep=(?), totalRep=(?) WHERE name LIKE (?)',
+                  [dailyRep, totalRep, username])
         id = data[0]['id']
     else:
         cur = c.execute("INSERT INTO user (name, dailyRep, totalRep) VALUES (?,?, ?)", [username, dailyRep, totalRep])
         id = cur.lastrowid
     conn.commit()
-    return id;
+    return id
 
 # given a song name and unique songid, update the song's last play and rep if it exists or insert the song into the db
 # returns the songid of the song
 def insertSong(name, id, rep):
     data = c.execute('SELECT * FROM song WHERE name LIKE (?) and id=(?)', [name, id]).fetchall()
     if data:
-        c.execute('UPDATE song SET LastPlay = (?) AND rep=(?) WHERE id =(?)', [datetime.datetime.now(), rep, id])
+        c.execute('UPDATE song SET LastPlay = (?), rep=(?) WHERE id =(?)', [datetime.datetime.now(), rep, id])
     else:
         c.execute('INSERT INTO song (Name, Rep, LastPlay, Id) VALUES (?, ?, ?, ?)',
                         [name, rep, datetime.datetime.now(), id])
         conn.commit()
-    return id;
+    return id
 
 # given an artist name, insert it into the db if it's unique, otherwise update the artists' rep
 # returns the artistid of the artist
@@ -55,36 +56,36 @@ def addUserSongRelation(userid, songid):
     data = c.execute('SELECT * FROM user_song WHERE userId = (?) AND songId = (?)', [userid, songid]).fetchall()
     if data:
         count = data[0]['count']
-        c.execute('UPDATE user_song SET lastPlay = (?) AND count=(?) WHERE userId = (?) AND songId = (?)',
-                  [datetime.datetime.now(), count + 1])
+        c.execute('UPDATE user_song SET lastPlay = (?), count=(?) WHERE userId = (?) AND songId = (?)',
+                  [datetime.datetime.now(), count + 1, userid, songid])
     else:
-        c.execute('INSERT INTO user_song (artistId, songId, lastPlay, count) VALUES (?, ?)',
+        c.execute('INSERT INTO user_song (userId, songId, lastPlay, count) VALUES (?, ?, ?, ?)',
                   [userid, songid, datetime.datetime.now(), 1])
         conn.commit()
 
-# given a username, return the reputation of that user in a list [dailyRep, totalRep] or -1 if the user does not exist
+# given a username, return the reputation of that user in a list [dailyRep, totalRep] or false if the user does not exist
 def getUserRep(username):
     data = c.execute('SELECT * FROM user WHERE name LIKE (?)', [username]).fetchall()
     if data:
         return [data[0]['dailyRep'], data[0]['totalRep']]
     else:
-        return -1
+        return False
 
-# given a songid, return the reputation of that song or -1 if the song does not exist
+# given a songid, return the reputation of that song or false if the song does not exist
 def getSongRep(songid):
     data = c.execute('SELECT * FROM song WHERE id = (?)', [songid]).fetchall()
     if data:
         return data[0]['rep']
     else:
-        return -1
+        return False
 
-# given an artist name, return the reputation of that artist or -1 if the artist does not exist
+# given an artist name, return the reputation of that artist or false if the artist does not exist
 def getArtistRep(artist):
     data = c.execute('SELECT * FROM artist WHERE name LIKE (?)', [artist]).fetchall()
     if data:
         return data[0]['rep']
     else:
-        return -1
+        return False
 
 # given an artist name, return all songs in the db by that artist
 def getArtistSongs(artist):
@@ -105,6 +106,7 @@ def getArtistSongs(artist):
     else:
         return False
 
+# given a username returns a list of dictionaries, each dictionary representing a song played by the user
 def getUserSongs(username):
     data = c.execute('SELECT * FROM user WHERE name LIKE (?)', [username]).fetchall()
     if data:
@@ -112,18 +114,14 @@ def getUserSongs(username):
         data2 = c.execute('SELECT * FROM song s INNER JOIN user_song u ON u.userId = ? WHERE s.id = u.songId',
                           [data[0]['id']]).fetchall()
         if data2:
-            songs = []
-            # add each of the artists' songs to the list
-            for d in data2:
-                songs.extend([d['name']])
-            return songs
+            return data2
         else:
             return False
     else:
         return False
 
 # FOR INTERNAL USE ONLY
-# makes db calls return a dictionary of results
+# makes db calls return a list of dictionaries instead of random shit
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
         for idx, value in enumerate(row))
@@ -131,18 +129,78 @@ def make_dicts(cursor, row):
 # EXAMPLE FUNCTION: This function shows how to add artists and songs to the db, when adding songs you must also add a
 # relation between all of the songs artists
 # given a list of artists, a song name, and unique song id, insert all of the artists and the song into the dbs
-def insertArtistSong(artists, song, songid):
+def insertArtistSong(artists, song, songid, rep):
     artistids = []
-    insertSong(song, songid)
+    songRep = getSongRep(songid)
+    if (songRep):
+        insertSong(song, songid, rep + songRep)
+    else:
+        insertSong(song, songid, rep)
     for artist in artists:
-        artistids.append(insertArtist(artist))
+        artistRep = getArtistRep(artist)
+        artistids.append(insertArtist(artist, artistRep))
     for artistid in artistids:
-        addRelation(artistid, songid)
+        addArtistSongRelation(artistid, songid)
 
+# EXAMPLE FUNCTION: This function shows how to add a user's songs to the db, when adding their songs you must also add
+# a relation between the user and song
+# given a username, song, and songid, insert the user and song into the db
+def insertUserSong(username, song, songid):
+    rep = getSongRep(songid)
+    if not rep:
+        insertSong(song, songid, BASEREP)
+        rep = BASEREP
+    userRep = getUserRep(username)
+    if (userRep):
+        newRep = userRep[0] + rep
+        userid = insertUser(username, newRep, userRep[1])
+    else:
+        userid = insertUser(username, rep, BASEREP)
+    addUserSongRelation(userid, songid)
+
+# how to connect to the database and initalize variables, the cursor and connection are need for the functions to work,
+# you need to figure out how to give each of the functions access to both the connection and the cursor
 conn = sqlite3.connect('song.db')
 conn.row_factory = make_dicts
 c = conn.cursor()
 
-c.insertUser("wonnor", 15, 10);
-c.insertSong("No Type", 10);
+# deletes all rows from all databases before every run
+c.execute('DELETE FROM user')
+c.execute('DELETE FROM artist')
+c.execute('DELETE FROM song')
+c.execute('DELETE FROM user_song')
+c.execute('DELETE FROM artist_song')
 
+# test cases
+
+BASEREP = 5
+firstSong = "Throw some mo"
+firstArtists = ["Nikki Minaj", "Rae Sremmurd"]
+firstUser = "wonnor"
+secondSong = "No Type"
+secondArtists = ["Rae Sremmurd"]
+secondUser = "nhoran"
+
+# Connor adds first song
+insertArtistSong(firstArtists, firstSong, 1, 10)
+insertUserSong(firstUser, firstSong, 1)
+
+# Connor adds second song
+insertArtistSong(secondArtists, secondSong, 2, 10)
+insertUserSong(firstUser, secondSong, 2)
+
+# Nate adds second song
+insertArtistSong(secondArtists, secondSong, 2, 15)
+insertUserSong(secondUser, secondSong, 2)
+
+# Connor plays second song again
+insertUserSong(firstUser, secondSong, 2)
+
+print firstUser, "has a reputation of", getUserRep(firstUser), "and has added:", getUserSongs(firstUser)
+print secondUser, "has a reputation of", getUserRep(secondUser), "and has added:", getUserSongs(secondUser)
+
+print firstSong, "has a reputation of", getSongRep(1)
+print secondSong, "has a reputation of", getSongRep(2)
+
+print "Rae Sremmurd's songs:", getArtistSongs("Rae Sremmurd")
+print "Nikki Minaj's songs:", getArtistSongs("Nikki Minaj")
